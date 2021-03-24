49917dd2092280fcdb043b0431b26a2859a57d65
Martin Sutherland <martinsutherland1@gmail.com>
2021-03-24 18:28:10 +0000

    Extensions completed

---
+++ app/controllers/controller.py
@@ -0,0 +1,20 @@
+from app import app
+from flask import render_template
+from app.models.customer_orders import orders
+
+
+
+@app.route('/orders')
+def index():
+    return render_template('index.html', title="Home", orders=orders)
+
+@app.route('/orders/<index>')
+def show(index):
+    return render_template('order.html', order=orders[int(index)])
+    
+
+
+
+
+
+    
---
+++ app/models/customer_orders.py
@@ -0,0 +1,12 @@
+from app.models.order import *
+
+
+
+customer1 = Order("Ted Mosby", "2/2/21", "Cowboy boots - Red", 1)
+customer2 = Order("Lily Aldrin", "5/2/21", "Leather Jacket - Black", 1)
+customer3 = Order("Barney Stinson", "12/2/21", "Slim Suit - Navy", 2)
+customer4 = Order("Robin Scherbatsky", "25/2/21", "Snow shoes - Blue", 1)
+customer5 = Order("Marshall Eriksson", "14/3/21", "Sweater Vest - Green", 1)
+customer6 = Order("Tracy McConnell", "17/3/21", "Leather driving gloves - Black", 1)
+orders = [customer1, customer2, customer3, customer4, customer5, customer6]
+
